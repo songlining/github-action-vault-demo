a407f5127e3f080e0d70e206c2170459391a5da9
Larry Song <1631094+songlining@users.noreply.github.com>
2025-07-08 14:07:29 +1000

    test

--- vault_commands.sh
+++ vault_commands.sh
@@ -18,7 +18,7 @@ vault write auth/jwt/role/myproject-github-role -<<EOF
 {
   "role_type": "jwt",
   "user_claim": "aud",
-  "bound_audiences": ["https://github.com/songlining/github-action-vault-demo__dev"],
+  "bound_audiences": ["https://github.com/songlining/github-action-vault-demo"],
   "bound_claims": {
     "repository": "songlining/github-action-vault-demo"
   },
